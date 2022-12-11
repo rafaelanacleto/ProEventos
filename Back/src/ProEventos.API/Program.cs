8b9ac8badebb8a2f1718929f9ae3ff3b8f1ea0ff
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2022-12-11 19:17:36 -0300

    atualizando project para dotnet 6

--- Back/src/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/Program.cs
@@ -15,8 +15,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
