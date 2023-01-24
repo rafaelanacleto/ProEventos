8c79da2bfd34fff57e0c3fe0b950627afa54bbd6
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-01-23 21:04:25 -0300

    Database and Migrations

--- Back/src/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(con => con.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
