6bd2aaf0325fdd81fb56af15bd2afa1d3be965e3
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-01-31 21:19:54 -0300

    atualizacao do context

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers;
@@ -6,56 +7,42 @@ namespace ProEventos.API.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
-{  
+{
 
-    public IEnumerable<Evento> _evento = new Evento[] {
-        new Evento() {
-        Id = 1,
-        DataEvento = "18/01/2023",
-        ImagemURL = "imagem.png",
-        Local = "Esteio",
-        Lote = "12",
-        QtdPessoas = 2,
-        Tema = "Angular 12"
-       },
-        new Evento() {
-        Id = 2,
-        DataEvento = "22/01/2023",
-        ImagemURL = "imagem.png",
-        Local = "GTI",
-        Lote = "2",
-        QtdPessoas = 22,
-        Tema = "C# 12"
-       }
-    };
+    private readonly DataContext _context;
+
+    public EventoController(DataContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {
-       return _evento;
+        return 
     }
 
     [HttpGet("{id}")]
     public IEnumerable<Evento> GetById(int? id)
     {
-        return _evento.Where(evento => evento.Id == id);         
+        return _evento.Where(evento => evento.Id == id);
     }
 
     [HttpPost]
     public IEnumerable<Evento> Post(Evento model)
     {
-       return _evento.Append(model);
+        return _evento.Append(model);
     }
 
     [HttpPut("{id}")]
     public string Put(int id)
     {
-       return $"Put - {id}";
+        return $"Put - {id}";
     }
 
     [HttpDelete("{id}")]
     public string Delete(int id)
     {
-       return $"Delete - {id}";
+        return $"Delete - {id}";
     }
 }
