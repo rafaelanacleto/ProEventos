d50d6f4724017fc359d647a3a6d1b5b5febf5cdd
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-02-06 11:23:39 -0300

    refatoracao nomes do arquivo css para scss

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
 
