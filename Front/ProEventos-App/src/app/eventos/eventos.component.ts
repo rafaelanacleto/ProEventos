a91f42d40dac6691b7961aad843fcfaa0ef4cee6
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-02-07 11:33:38 -0300

    ng for laço dentro do angular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventos : any = [
+    {
+      Tema: 'angular',
+      Local: 'Esteio-RS',
+      Quantidade : 1
+    },
+    {
+      Tema: 'C#',
+      Local: 'Gravataí-RS',
+      Quantidade : 2
+    },
+    {
+      Tema: 'Javascript',
+      Local: 'Porto Alegre-RS',
+      Quantidade : 3
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
