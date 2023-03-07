a20a8a325f4d1ff45f2fdef2f549a61a66ec0440
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-03-07 14:37:23 -0300

    Criado ProEvento Context

---
+++ Back/src/ProEventos.Persistence/ProEventosContext.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain.Models;
+
+namespace ProEventos.Persistence
+{
+    public class ProEventosContext : DbContext
+    {
+        public ProEventosContext(DbContextOptions<ProEventosContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Palestrante> Palestrantes { get; set; }
+        public DbSet<PalestranteEvento> PalestranteEventos { get; set; }
+        public DbSet<Lote> Lotes { get; set; }
+        public DbSet<RedeSocial> RedeSociais { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // modelBuilder.Entity<UserRole>(userRole =>
+            //     {
+            //         userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            //         userRole.HasOne(ur => ur.Role)
+            //             .WithMany(r => r.UserRoles)
+            //             .HasForeignKey(ur => ur.RoleId)
+            //             .IsRequired();
+
+            //         userRole.HasOne(ur => ur.User)
+            //             .WithMany(r => r.UserRoles)
+            //             .HasForeignKey(ur => ur.UserId)
+            //             .IsRequired();
+            //     }
+            // );
+
+            modelBuilder.Entity<PalestranteEvento>()
+                .HasKey(PE => new { PE.EventoId, PE.PalestranteId });
+        }
+
+
+    }
+}
