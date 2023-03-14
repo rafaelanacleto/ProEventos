019457cc97184b33ba04bd72dc74e4f29f14be21
rafaelanacleto <rafael.anacleto@7comm.com.br>
2023-03-14 15:31:29 -0300

    Finalizado interface eventos da application

--- Back/src/ProEventos.Application/Contratos/IEventosService.cs
+++ Back/src/ProEventos.Application/Contratos/IEventosService.cs
@@ -3,11 +3,15 @@ using ProEventos.Domain.Models;
 namespace ProEventos.Application.Contratos
 {
     public interface IEventosService
-    {  
-         //EVENTOS
-         Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrante);
-         Task<Evento[]> GetAllEventoAsync(bool includePalestrante);
-         Task<Evento> GetAllEventoAsyncById(int EventoID, bool includePalestrante);
+    {
+        //EVENTOS
+        Task<Evento> AddEventos(Evento model);
+        Task<Evento> UpdateEvento(int id, Evento model);
+        Task<bool> DeleteEvento(int id);
+
+        Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrante = false);
+        Task<Evento[]> GetAllEventoAsync(bool includePalestrante);
+        Task<Evento> GetAllEventoAsyncById(int EventoID, bool includePalestrante = false);
 
     }
 }
--- Back/src/ProEventos.Persistence/Repositories/IEventoPersist.cs
+++ Back/src/ProEventos.Persistence/Repositories/IEventoPersist.cs
@@ -5,9 +5,9 @@ namespace ProEventos.Persistence.Repositories
     public interface IEventoPersist
     {  
          //EVENTOS
-         Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrante);
+         Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrante = false);
          Task<Evento[]> GetAllEventoAsync(bool includePalestrante);
-         Task<Evento> GetAllEventoAsyncById(int EventoID, bool includePalestrante);
+         Task<Evento> GetAllEventoAsyncById(int EventoID, bool includePalestrante = false);
 
     }
 }
