f0f4537fa27aa284f5a75f486abe60c1c3d5024d
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-03-18 19:56:56 -0300

    Ajustado banco de dados

--- Back/src/ProEventos.API/Program.cs
+++ Back/src/ProEventos.API/Program.cs
@@ -4,6 +4,7 @@ using ProEventos.Persistence.Context;
 using ProEventos.Application.Contratos;
 using ProEventos.Application;
 using ProEventos.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 builder.Services.AddDbContext<ProEventosContext>(con => con.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
