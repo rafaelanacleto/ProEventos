046bb9829135dce077a53fbd6c562913a89e8bde
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-07-18 09:54:32 -0300

    Adcionado detalhe evento e limpado evento

--- Front/ProEventos-App/src/app/app-routing.module.ts
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -4,9 +4,18 @@ import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventoDetalhesComponent } from './components/eventos/evento-detalhes/evento-detalhes.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 
 const routes: Routes = [
-  { path: 'eventos', component: EventosComponent },
+  {
+    path: 'eventos', component: EventosComponent,
+    children: [
+      { path: 'detalhe/:id' , component: EventoDetalhesComponent },
+      { path: 'detalhe' , component: EventoDetalhesComponent },
+      { path: 'lista' , component: EventoListaComponent }
+    ]
+  },
   { path: '', component: EventosComponent },
   { path: 'palestrantes', component: PalestrantesComponent },
   { path: 'dashboard', component: DashboardComponent },
--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
-import { Evento } from '../../models/Evento';
-import { EventoService } from '../../services/evento.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-eventos',
@@ -14,90 +11,11 @@ import { NgxSpinnerService } from "ngx-spinner";
 export class EventosComponent implements OnInit {
 
   nome:string = 'Eventos';
-  public eventos: Evento[] = [];
-  public eventosFiltrados: Evento[] = [];
-  larguraImagem: number = 50;
-  margemImagem: number = 0.5;
-  mostrarImagem: boolean = false;
-  private _filtroLista : string = '';
-  modalRef?: BsModalRef;
-  message?: string;
 
-  constructor(private eventoService: EventoService,
-              private modalService: BsModalService,
-              private toastr: ToastrService,
-              private spinner: NgxSpinnerService) {
+  constructor(private modalService: BsModalService, private toastr: ToastrService) {
   }
 
   ngOnInit() {
-
-    /** spinner starts on init */
-    this.spinner.show();
-
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 1200);
-
-    this.getEventos();
-  }
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  showSuccess() {
-    this.toastr.success('Hello world!', 'Toastr fun!');
-  }
-
-  confirm(): void {
-    this.message = 'Confirmed!';
-    this.modalRef?.hide();
-    this.toastr.success('O evento foi deletado com sucesso!', 'Deletado!');
-  }
-
-  decline(): void {
-    this.message = 'Declined!';
-    this.modalRef?.hide();
-    this.toastr.error('Exclusão Cancelada!', 'Informação');
-  }
-
-  public getEventos(): void {
-
-    this.eventoService.getAllEvento()
-      .subscribe(
-        {
-          next: (v) => this.eventos = v,
-          error: (e) => this.toastr.error('Erro ao buscar os Eventos!', 'Erro 500'),
-          complete: () => this.eventosFiltrados = this.eventos
-        }
-      );
-  }
-
-  public onToggleImage(): void {
-    this.mostrarImagem = !this.mostrarImagem;
-  }
-
-  filtrarEventos(filtrarPor : string): any {
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      (evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    );
-  }
-
-  public get filtroLista(): string {
-    return this._filtroLista;
-  }
-
-  public cleanFiltroLista() : any {
-    this._filtroLista = '';
-    this.getEventos();
-  }
-
-  public set filtroLista(para : string) {
-    this._filtroLista = para;
-    this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this._filtroLista) : this.eventos;
   }
 
 }
