d8f4e805e2dda124a23940d682044443e4edb7a5
M319209 <rafael.anacleto@SL094>
2023-08-21 22:08:09 -0300

    Desenvolvimento formulario registration

--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
+
+  form: any = FormGroup;
+  
+  constructor(
+    private fb: FormBuilder,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService,
+  ) { }
+  
+  ngOnInit() {
+    this.validation();
+  }
+
+  public validation() : void {
+
+    this.form = this.fb.group(
+      {
+        firstName: ['', [Validators.required, Validators.minLength(5)]],
+               
+      }
+    );
+  }
+
 
 }
