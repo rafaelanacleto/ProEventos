12beee7bb2029a3b7491efec50445b9d716b6d27
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-08-27 13:34:02 -0300

    feat: Iniciando deseonvilvemento do perfil do usuario

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { ValidatorField } from 'src/app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
+  form: any = FormGroup;
+
   nome:string = 'Perfil';
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService,
+  ) { }
 
   ngOnInit() {
   }
+
+
+  public validation() : void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch("senha", "confirmeSenha")
+    };
+
+
+    this.form = this.fb.group(
+      {
+        titulo: ['', [Validators.required, Validators.minLength(5)]],
+
+      }, formOptions
+    );
+  }
+
+
+
 
 }
