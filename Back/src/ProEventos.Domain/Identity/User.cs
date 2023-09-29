84ed7f83b36521f475518bbe20dcd687642e4052
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2023-09-29 20:06:56 -0300

    Add identity na camada persistence

--- Back/src/ProEventos.Domain/Identity/User.cs
+++ Back/src/ProEventos.Domain/Identity/User.cs
@@ -9,12 +9,12 @@ namespace ProEventos.Domain.Identity
 {
     public class User : IdentityUser<int>
     {
-        public string PrimeiroNome { get; set; }
-        public string UltimoNome { get; set; }
-        public Titulo Titulo { get; set; }
-        public string Descricao { get; set; }
+        public string? PrimeiroNome { get; set; }
+        public string? UltimoNome { get; set; }
+        public Titulo? Titulo { get; set; }
+        public string? Descricao { get; set; }
         public Funcao Funcao { get; set; }
-        public string ImagemURL { get; set; }
-        public IEnumerable<UserRole> UserRoles { get; set; }
+        public string? ImagemURL { get; set; }
+        public IEnumerable<UserRole>? UserRoles { get; set; }
     }
 }
--- Back/src/ProEventos.Domain/Identity/UserRole.cs
+++ Back/src/ProEventos.Domain/Identity/UserRole.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ProEventos.Domain.Identity
 {
-    public class UserRole
+    public class UserRole : IdentityUserRole<int>
     {
         public User User { get; set; }
         public Role Role { get; set; }
