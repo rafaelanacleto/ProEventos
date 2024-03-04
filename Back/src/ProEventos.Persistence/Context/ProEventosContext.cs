429a64d2dd9f830197d2efe69decf0684031f23c
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-03-04 10:49:47 -0300

    Adcionando users na tabela e identity

--- Back/src/ProEventos.Persistence/Context/ProEventosContext.cs
+++ Back/src/ProEventos.Persistence/Context/ProEventosContext.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain.Identity;
 using ProEventos.Domain.Models;
 
 namespace ProEventos.Persistence.Context
 {
-    public class ProEventosContext : DbContext
+    public class ProEventosContext : IdentityDbContext<User, Role, int, 
+                                                IdentityUserClaim<int>, UserRole,IdentityUserLogin<int>, 
+                                                IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public ProEventosContext(DbContextOptions<ProEventosContext> options) : base(options)
         {
